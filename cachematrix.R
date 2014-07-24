e60b94b25b6a1b7d2053e2998e7b5217c638a51f
blackfishnw <randy.kelley@me.com>
2014-07-24 15:47:43 -0700

    Incremental commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
+
+## function makeCacheMatrix creates a cache of a matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+    mi <- NULL  
+    
+    # function to Set a copy of the matrix
+    set <- function(y) {
+      x <<- y
+      mi <<- NULL
+    }
+    
+    get <- function() x  #get the matrix
+    setimatrix <- function(solve) mi <<- solve
+    getimatrix <- function() mi
+    
+    # return a list containing functions (methods of cache object so to speak)
+    list(set = set, get = get, setimatrix = setimatrix, getimatrix = getimatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the matrix of 'x', if it:
+##  is not in the cache, and has not changed. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    m <- x$getimatrix()
+    
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    
+    data <- x$get()
+    m <- solve(data, ...) ## Return a matrix that is the inverse of 'x'
+    x$setimatrix(m)
+    
+    m  #return
 }
